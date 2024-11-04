c2d991536f982df3609e8532662b89819baa1dc2
pransh1 <mauryapransh2@gmail.com>
2024-11-04 13:41:12 +0530

    updated my project

---
+++ server/config/dbConnections.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async() => {
+  try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log(`mongodb connection successful || HOST ${connect.connection.host}`);
+  }
+  catch(error){
+    console.log("mongodb connection failed",error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,64 @@
+//Framework Configuration
 const express = require("express");
-const connectDB = require("./config/dbConnections");
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
+const hbs = require("hbs");
+const path = require("path");
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
 
-connectDB();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
 app.use(errorHandler);
 
-app.get('/', (req,res) => {
-  res.send("working");
+app.use('/api/register', require("./routes/userRoutes"));
+
+
+// ERROR handling middleware
+app.use(errorHandler);
+
+app.set('view engine', 'hbs');
+
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+
+app.get("/home",(req,res)=>{
+    res.render("home",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
 })
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+app.get("/allusers",(req,res)=>{
+    res.render("users",{
+        users: [
+            { username: "Parth", date: "23-10-2024", subject: "Maths" },
+            { username: "Aarav", date: "23-10-2024", subject: "Science" },
+            { username: "Ishita", date: "23-10-2024", subject: "History" }
+        ]
+    })
 })
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
+// APP CONFIG START
+app.listen(port, () =>{
+    console.log(`Server running in port http://localhost:${port}`);
+});
